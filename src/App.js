8334bad1f1031ac80a073155ce929eed425b730e
siddharthkoundal <sksiddharth27@gmail.com>
2022-05-24 15:06:07 +0530

    first styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import HomePage from "./components/homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.component";
 
 function App() {
   return (
