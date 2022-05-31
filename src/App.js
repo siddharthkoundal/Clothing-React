d4be1e515bb857c511ec9548ca6ce1099b8f07dd
siddharthkoundal <sksiddharth27@gmail.com>
2022-05-31 21:51:56 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
 
 function HomePage() {
